6471518522d28ee18d5368f80ef7c8f07d45ddee
Jackson Seim <jseim4699@gmail.com>
2025-08-11 11:25:43 -0500

    Initial Commit

---
+++ Personal Multi Use/Personal_Multi_UseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Multi_UseApp.swift
+//  Personal Multi Use
+//
+//  Created by Jackson Seim on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Multi_UseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Personal Multi UseTests/Personal_Multi_UseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Multi_UseTests.swift
+//  Personal Multi UseTests
+//
+//  Created by Jackson Seim on 8/11/25.
+//
+
+import Testing
+@testable import Personal_Multi_Use
+
+struct Personal_Multi_UseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
